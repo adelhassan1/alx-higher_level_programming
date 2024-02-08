ab6ed66dd2262fc949f72ee089207a4888aab90c
adelhassan1 <tomasander12@gmail.com>
2024-02-08 21:16:33 +0200

    class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,6 +54,7 @@ class Square:
         """prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
+            return
         for i in range(self.__position[1]):
             print()
         for i in range(self.__size):

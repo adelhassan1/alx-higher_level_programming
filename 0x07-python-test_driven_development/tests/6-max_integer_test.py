7806a25f641bbe28da450ebddc4e3d1bdc21ada1
adelhassan1 <tomasander12@gmail.com>
2024-02-16 00:08:04 +0200

    tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,7 +5,9 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    """Tests for max integer"""
+
+    def test_import(self):
+        self.assertTrue(__import__('6-max_integer').max_integer)
 
     def test_integer(self):
         """Test if it gives must integer"""
@@ -13,6 +15,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([3, 5, 6, 3, 9, 2]), 9)
         self.assertEqual(max_integer([-1, -2, -4, -5, -9]), -1)
         self.assertEqual(max_integer([-1, 4, -9]), 4)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer({0: 'a', 1: 'z', 2: 'c'}), 'z')
 
     def test_types(self):
         """Make sure type errors are raised"""
@@ -23,3 +27,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertRaises(TypeError, max_integer, None)
         self.assertRaises(TypeError, max_integer, True)
         self.assertRaises(TypeError, max_integer, -3)
+        self.assertRaises(KeyError, max_integer, {'a': 2, 'b':1, 'c':3})
+
+if __name__ == '__main__':
+    unittest.main()

c685953c840b2d9574dfcd4064a401381eaecf31
adelhassan1 <tomasander12@gmail.com>
2024-02-22 04:52:09 +0200

    json

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,5 @@ def read_file(filename=""):
         filename
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read().rstrip())
+        text = f.read()
+        print(text, end='')
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,5 +8,5 @@ def append_write(filename="", text=""):
         filename
         text
     """
-    with open(filename, 'r+', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)

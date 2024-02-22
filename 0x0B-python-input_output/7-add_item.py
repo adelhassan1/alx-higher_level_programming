102cd95167debc00f35a964d8d7d3c2a8959a62a
adelhassan1 <tomasander12@gmail.com>
2024-02-22 04:55:19 +0200

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,14 @@ import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+num = len(sys.argv)
+
+
 try:
     mylist = load_from_json_file('add_item.json')
 except TypeError:
     mylist = []
 
-for i in range(1, len(sys.argv)):
+for i in range(1, num):
     mylist.append(sys.argv[i])
 save_to_json_file(mylist, 'add_item.json')
